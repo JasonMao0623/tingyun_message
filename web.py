8d6553f3ce519467507d62ce89f9fc53d04a076d
JasonMao0623 <470304624@qq.com>
2018-09-10 09:01:32 +0800

    09091255

--- web.py
+++ web.py
@@ -3,10 +3,10 @@ app =Flask(__name__)
 import time
 import log_out
 import we_chat
-def utils(mobiles_array,content):
+def utils(mobiles_array,content,sendkey):
     for mobile in mobiles_array:
         if content and mobile:
-            we_chat.sendwx(content, "3288-cddd2195e76cfba892c071d7cfac279f")
+            we_chat.sendwx(content, sendkey)
             content_value = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + " 推送成功 " + " 内容：" + \
                             request.form["content"]
             log_out.log_out(content_value)
@@ -21,7 +21,7 @@ def test_dataprocess():
     if request.method =="POST":
         mobiles_array = request.form["mobile"].split(",")
         content = request.form["content"]
-        res = utils(mobiles_array,content)
+        res = utils(mobiles_array,content,"3288-cddd2195e76cfba892c071d7cfac279f")
         return  res
     else:
         return "请求方法为POST",500
@@ -30,7 +30,7 @@ def tongyong_dataprocess():
     if request.method =="POST":
         mobiles_array = request.form["mobile"].split(",")
         content = request.form["content"]
-        res = utils(mobiles_array,content)
+        res = utils(mobiles_array,content,"5558-55f89f81d22f4b620fe52ce3dddb1463")
         return  res
     else:
         return "请求方法为POST",500
