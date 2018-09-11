57b6171a914d7e9d6bc5a78f1c3a3b1d3183f749
JasonMao0623 <470304624@qq.com>
2018-09-11 10:51:06 +0800

    09111050

--- web.py
+++ web.py
@@ -3,15 +3,13 @@ app =Flask(__name__)
 import time
 import log_out
 import we_chat
-def utils(content,sendkey):
+def utils(content,sendkey,name):
     if content:
         we_chat.sendwx(content, sendkey)
-        content_value = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + " 推送成功 " + " 内容：" + \
-                        request.form["content"]
+        content_value = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + " 推送成功 " + " 内容：" + content + "客户:"+name
         log_out.log_out(content_value)
     else:
-        content_value = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + " 推送失败 " + " 内容：" + \
-                        request.form["content"]
+        content_value = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + " 推送失败 " + " 内容：" + content + "客户:"+name
         log_out.log_out(content_value)
         return "推送失败请确认请求参数", 500
     return "推送成功", 200
@@ -22,7 +20,8 @@ def test_dataProcess():
         print(request.form)
         content = request.form["content"]
         key = str(request.form["key"])
-        res = utils(content,key)
+        name = str(request.form["name"])
+        res = utils(content,key,name)
         return  res
     else:
         return "请求方法为POST",500
@@ -33,7 +32,8 @@ def product_dataProcess():
         print(request.form)
         content = request.form["content"]
         key = str(request.form["key"])
-        res = utils(content,key)
+        name = str(request.form["name"])
+        res = utils(content,key,name)
         return  res
     else:
         return "请求方法为POST",500
