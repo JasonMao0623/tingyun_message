485ba75eaf87a42a7957c86059959e2ac2a1c271
JasonMao0623 <470304624@qq.com>
2019-05-17 08:42:25 +0800

    修改接口为自研接口

--- we_chat.py
+++ we_chat.py
@@ -10,3 +10,16 @@ def sendwx(content,sendKey):
     res = requests.post(url,data)
     # print(json.loads(res.text))
     return res
+def sendwxnew(content,sendKey,title):
+    url = "https://tingyun.leanapp.cn/push"
+    header={
+        "content-type":"application/json"
+    }
+    data={
+        "channelName": sendKey,
+        "text": content,
+        "title":title
+    }
+    res = requests.post(url,json.dumps(data),headers=header)
+    print(json.loads(res.text))
+    return res
--- web.py
+++ web.py
@@ -5,12 +5,12 @@ import log_out
 import we_chat
 def utils(content,sendkey,name):
     if content:
-        we_chat.sendwx(content, sendkey)
+        we_chat.sendwxnew(content,sendkey,name)
         content_value = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + " 推送成功 " + " 内容：" + content + "客户:"+name
-        log_out.log_out(content_value)
+        # log_out.log_out(content_value)
     else:
         content_value = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + " 推送失败 " + " 内容：" + content + "客户:"+name
-        log_out.log_out(content_value)
+        # log_out.log_out(content_value)
         return "推送失败请确认请求参数", 500
     return "推送成功", 200
 @app.route("/test/message",methods=["POST"])
@@ -18,8 +18,8 @@ def utils(content,sendkey,name):
 def test_dataProcess():
     if request.method =="POST":
         print(request.form)
-        template = "\n\n[![听云官网](https://saas.tingyun.com/static/website/images/ty-logo.png \"听云官网\")](https://www.tingyun.com \"听云官网\")\n\n**国内专业的应用性能监控平台**"
-        content = str(request.form["content"]) + template
+        # template = "\n\n[![听云官网](https://saas.tingyun.com/static/website/images/ty-logo.png \"听云官网\")](https://www.tingyun.com \"听云官网\")\n\n**国内专业的应用性能监控平台**"
+        content = str(request.form["content"])
         key = str(request.form["key"])
         name = str(request.form["name"])
         res = utils(content,key,name)
@@ -31,8 +31,8 @@ def test_dataProcess():
 def product_dataProcess():
     if request.method =="POST":
         print(request.form)
-        template ="\n\n[![听云官网](https://saas.tingyun.com/static/website/images/ty-logo.png \"听云官网\")](https://www.tingyun.com \"听云官网\")\n\n**国内专业的应用性能监控平台**"
-        content = str(request.form["content"])+template
+        # template ="\n\n[![听云官网](https://saas.tingyun.com/static/website/images/ty-logo.png \"听云官网\")](https://www.tingyun.com \"听云官网\")\n\n**国内专业的应用性能监控平台**"
+        content = str(request.form["content"])
         key = str(request.form["key"])
         name = str(request.form["name"])
         res = utils(content,key,name)
