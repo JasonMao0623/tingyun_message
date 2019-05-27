f525001ef1fe76fc3c9ce44d36e29e361e477f68
JasonMao0623 <470304624@qq.com>
2019-05-27 11:11:15 +0800

    修改接口为自研接口,兼容老接口

--- web.py
+++ web.py
@@ -6,11 +6,12 @@ import we_chat
 def utils(content,sendkey,name):
     if content:
         we_chat.sendwxnew(content,sendkey,name)
+        we_chat.sendwx(content,sendkey)
         content_value = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + " 推送成功 " + " 内容：" + content + "客户:"+name
-        # log_out.log_out(content_value)
+        log_out.log_out(content_value)
     else:
         content_value = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + " 推送失败 " + " 内容：" + content + "客户:"+name
-        # log_out.log_out(content_value)
+        log_out.log_out(content_value)
         return "推送失败请确认请求参数", 500
     return "推送成功", 200
 @app.route("/test/message",methods=["POST"])
